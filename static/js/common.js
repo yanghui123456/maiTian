c0b69a7d78f7f25d2e449c50c5869ff6de671da8
yanghui <879904649@qq.com>
2019-11-30 12:23:00 +0800

    删除没用的后台地址

--- static/js/common.js
+++ static/js/common.js
@@ -1,7 +1,7 @@
 /**
  * Created by Mr.Liu on 2019/4/19.
+ * 2019.11.30放到git
  */
- // window.serverIp = 'http://172.16.116.76:80' // 其他服务器
  window.serverIp = 'https://maitian-dev.chinacloudsites.cn' // 其他服务器
  // window.serverIp = 'http://mtssct.leiruitec.com'
  //window.sendCodeIp = 'http://mtssct.leiruitec.com'
